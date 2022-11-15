296e374a7088807ffdb7d54ddff989edc1d90d96
KamalChamlagain <kamalchamlagain15@gmail.com>
2022-11-15 07:20:58 +0545

    insert page created

--- resources/views/insertRead.blade.php
+++ resources/views/insertRead.blade.php
@@ -1,26 +1,38 @@
 @extends('welcome')
 @section('content')
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
+<center>
+  <!-- Button trigger modal -->
+  <button type="button" class="btn btn-outline-primary fw-bold fs-4 rounded-pill" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Add Record
+  </button>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-       <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-       <a href=""><button type="button" class="btn btn-primary">Save</button></a>
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form action="" method="">
+            <div class="mb-2">
+              <input type="text" name="name" class="form-control" placeholder="Product Name">
+            </div>
+            <div class="mb-2">
+              <input type="text" name="price" class="form-control" placeholder="Price">
+            </div>
+            <div class="mb-2">
+              <input type="file" name="name" class="form-control" placeholder="Product Name">
+            </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save</button>
+        </div>
+        </form>
       </div>
     </div>
   </div>
-</div>
+</center>
 @endsection
